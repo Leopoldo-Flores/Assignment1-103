592f38d1037ff87da43c481498c39957e9413f87
Leo <83550516+Leopoldo-Flores@users.noreply.github.com>
2021-05-23 23:29:21 -0700

    Add files via upload

---
+++ assignment1/script1.js
@@ -0,0 +1,43 @@
+var a,b,c,d,e,f,g,h,i,j;
+
+let fullName = "Leopoldo Javier Flores";
+let firstName = "Leopoldo";
+let middleName = "Javier";
+let lastName = "Flores";
+let address = "5833 Loveland";
+let zipCode = 90201;
+let nationality = "Mexican-American";
+let phoneNumber = "(323) 444-8888";
+let email = "leopoldoflores2002@gmail.com";
+let dateOfBirth = "05/25/2002";
+a=1;
+b=2;
+c=3;
+d=4;
+e=5;
+f=6
+g=7;
+h=8;
+i=9;
+j=10;
+
+document.write('Full Name: ' + fullName + '<br>');
+document.write('First Name: ' + firstName  + '<br>');
+document.write('Middle Name: ' + middleName + '<br>');
+document.write('Last Name: ' + lastName + '<br>');
+document.write('Address: ' + address + '<br>');
+document.write('Zip Code: ' + zipCode + '<br>');
+document.write('Nationality: ' + nationality + '<br>');
+document.write('Phone Number: ' + phoneNumber + '<br>');
+document.write('Email: ' + email + '<br>');
+document.write('Date Of Birth: ' + dateOfBirth  + '<br>');
+document.write(a + '<br>');
+document.write(b + '<br>');
+document.write(c + '<br>');
+document.write(d + '<br>');
+document.write(e + '<br>');
+document.write(f + '<br>');
+document.write(g + '<br>');
+document.write(h + '<br>');
+document.write(i + '<br>');
+document.write(j + '<br>');
